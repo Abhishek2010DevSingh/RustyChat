bd9357a24e89079d28c7b8d5ea577ec4da9a5752
Abhishek2010DevSingh <=>
2024-12-01 20:49:58 +0530

    first commit

---
+++ src/model.rs
@@ -0,0 +1,12 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Default)]
+pub struct Conversation {
+    pub messages: Vec<Message>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Message {
+    pub user: bool,
+    pub text: String,
+}
